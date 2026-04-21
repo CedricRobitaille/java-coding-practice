8bb9d2a98130adab591e58b3a749775ea00b0076
Cedric <crubbertire@rogers.com>
2026-04-21 15:52:59 -0700

    init commit

---
+++ perm-missing-element/Main.java
@@ -0,0 +1,31 @@
+
+// Given a list of numbers starting from 1
+// In this list we have a missing element
+
+// The aim:
+// find the missing number
+
+// Note: 
+// the numbers are not in order
+
+// Given:
+// An array of size 'N'
+// Between [1, n+1]
+
+// Ie:
+// [2,3,1,5]
+// n = 4
+// range = [1,5]
+
+public class Main {
+
+  public static void main(String[] args) {
+      int[] arr = {3,2,1,5};
+      int result = solution(arr);
+      System.out.println(result);
+  }
+
+  public static int solution(int[] a) {
+    return 1;
+  }
+}
