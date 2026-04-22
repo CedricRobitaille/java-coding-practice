d238d80e191b8aedc6308b3dad829bc0e99553a0
Cedric <crubbertire@rogers.com>
2026-04-21 20:18:56 -0700

    Init commit

--- cyclic-rotation/Main.java
+++ cyclic-rotation/Main.java
@@ -1,5 +1,24 @@
+// Problem:
+// Input array where we need to preform a cyclic operation.
+// Shift every element of the array to the right.
+// The last element in the array should return to the beginning of the list.
+
+// Each cycle is defined by K.
+// K = 1 performs 1 cyclic rotation
+
+// Goal:
+// The function solution (a, k)
+// This function reutns an array with contents rotated k times to the right.
+
+
+
 public class Main {
   public static void main(String[] args) {
       
+  }
+
+  public static int[] solution(int[] a, int k) {
+    int[] cycledArr = {};
+    return cycledArr;
   }
 }
