83783ff72c525b25e79d18994f1501468b474791
Cedric <crubbertire@rogers.com>
2026-04-21 21:01:57 -0700

    init commit

---
+++ frog-river-one/Main.java
@@ -0,0 +1,23 @@
+// Problem:
+// Given an assorted array like [1,3,1,4,5,3,2,1]...
+// Where the index of the array represents time, how long it will take for every int between 1 and n to appear.
+
+// Notes:
+// Will need function: solution(x, a)
+// x is the size of the river
+// a is the input list
+// x and n have a limit of 100,000.
+// If every int is not found, return -1
+
+public class Main {
+  public static void main(String[] args) {
+    int[] inputArr = {2,4,1,2,3,3,5};
+    System.out.println(solution(5, inputArr));
+  }
+
+  public static int solution(int x, int[] a) {
+    
+
+    return -1;
+  }
+}
