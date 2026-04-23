ddbbe6789151ecc1847d0f4626e10625bc5ed8f4
Cedric <crubbertire@rogers.com>
2026-04-22 17:26:20 -0700

    init commit

---
+++ fish/Main.java
@@ -0,0 +1,33 @@
+// Problem:
+// You have a line of fish
+// Each fish have varying directions (L/R), and sizes.
+// If a fish meets another fish, and is bigger, it eats the smaller fish.
+// The aim is to find out how many fish survive.
+
+// Rules:
+// Fish swimming left = 0
+// Fish swimming right = 1
+// First input is an array of directions [0,1,1,0,0]
+// Second input is an array of sizes [6,2,3,2,7]
+
+// Note:
+// Function solution(a, b)
+// a is an array of directions
+// b is an array of sizes
+// Should return an int of remaining fish.
+
+// CPU: O(n)
+// MEM: O(n)
+
+public class Main {
+  public static void main(String[] args) {
+    int[] inputDirections = {};
+    int[] inputSizes = {};
+    System.out.println(solution(inputDirections, inputSizes));
+  }
+
+  public static int solution(int[] a, int[] b) {
+
+    return 0;
+  }
+}
